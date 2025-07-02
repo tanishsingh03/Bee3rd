88ecd7a44f108fd9054e7a8409f144884b714307
Tanish Singh <luffylearning03@gmail.com>
2025-07-02 15:43:04 +0530

    lec1 update

--- lec-1/eventloop.js
+++ lec-1/eventloop.js
@@ -6,6 +6,25 @@ const fs = require('fs');
 setImmediate(() => {
     console.log("Immediate 1");
 }); 
+
+function dosometask() {
+    return new Promise((resolve, reject) => {
+        resolve("Task Result");
+    });
+}
+
+dosometask()
+    .then((res) => {
+        console.log("Promise Resolved:", res);
+    })
+    .catch((err) => {
+        console.error("Error:", err);
+    });
+
+process.nextTick(() => {
+    console.log("Next Tick 1");
+});
+
 fs.readFile('sample.txt', 'utf-8', (err, data) => {
     console.log("File content:", data);
     setTimeout(() => {

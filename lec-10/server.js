8b335971e88e90bff6930790ea12425dcd53c4e1
Tanish Singh <luffylearning03@gmail.com>
2025-07-23 14:23:58 +0530

    arrignment commit

---
+++ lec-10/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Server is working!');
+});
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
